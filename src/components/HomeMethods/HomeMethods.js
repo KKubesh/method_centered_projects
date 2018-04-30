a423d119137a373a692a2c8b8002207514772be2
Kam Kubesh <kube0057@umn.edu>
2018-04-30 14:46:43 -0500

    Placed in the HomeMethods component

---
+++ src/components/HomeMethods/HomeMethods.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class HomeMethods extends Component {
+
+    render() {
+        return(
+            <div>
+                This is where where we will make all the cards of each method
+            </div>
+        )
+    }
+}
+
+export default HomeMethods;
