503352f93791b5b4009bbc47b65c2a21d1a9782c
Kam Kubesh <kube0057@umn.edu>
2018-05-04 13:53:41 -0500

    major changes to all methods styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Header = ({ title }) => (
-  <div className="bannerback">
-    <div>
+  <div>
+    <div className="center-item">
       <h1>{ title }</h1>
+    </div>
+    <div className="bannerback">
     </div>
   </div>
 );
--- src/components/UserMethodItem/UserMethodItem.js
+++ src/components/UserMethodItem/UserMethodItem.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import IconButton from 'material-ui/IconButton';
 import Dialog from 'material-ui/Dialog';
-import Icon from 'material-ui/Icon';
+import { Queue } from '@material-ui/icons';
+import Card, { CardMedia, CardHeader } from 'material-ui/Card';
+import Button from 'material-ui/Button';
 
 const styles = {
     dialogStyling: {
         width: '80%',
-        minWidth: '300px'
+        minWidth: '300px',
+        padding: '30px',
     },
     root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
+        display: 'flex'
+    },
+    rootCard: {
+        padding: '10px',
+        margin: '5px',
     }
 }
 
@@ -33,14 +38,30 @@ class AdminMethodItem extends Component {
         
         return(
             <div style={styles.root}>
-                    <img onClick={this.handleOpen} alt="method" height="250" width="250" src={this.props.method.image}/>
+                <Card style={styles.rootCard} title={this.props.method.title}>
+                    <CardHeader 
+                        title={this.props.method.title}
+                        action={
+                            <IconButton>
+                              <Queue />
+                            </IconButton>
+                        }
+                    />
+                    <CardMedia
+                        style={{height: '290px', width: '290px'}}
+                        image={this.props.method.image}
+                        onClick={this.handleOpen}
+                    />
+                    
+                </Card>
                 <Dialog
                     title={this.props.method.title}
                     open={this.state.open}
                     onClose={this.handleClose}
+                    style={styles.dialogStyling}
                 >
                 <img alt="method" height="250" width="250" src={this.props.method.image}/>
-                <p>This is the existing method for the admin editing. Keep on writing more stuff and add more stuff in here. </p>
+                <p>This is the existing method extended. Keep on writing more stuff and add more stuff in here. </p>
                 </Dialog>
             </div>
         )
