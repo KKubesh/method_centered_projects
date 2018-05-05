fe5b4ec1e71bcbf5fe632d5e0589b80ebaf972b5
Kam Kubesh <kube0057@umn.edu>
2018-05-05 15:00:35 -0500

    router copied for put

--- server/routes/method.router.js
+++ server/routes/method.router.js
@@ -35,6 +35,28 @@ router.post('/', (req, res) => {
         });
 });
 
+router.put('/:id', (req, res) => {
+    let m = req.body;
+    const queryText = `UPDATE method SET 
+        title = $1,
+        statements = $2,
+        description = $3,
+        time_amount = $4,
+        diffculty = $5,
+        need = $6,
+        participants = $7,
+        steps = $8,
+        image = $9
+        WHERE id = $10;`;
+    pool.query(queryText, [m.title, m.statements, m.description, m.time_amount, 
+        m.diffculty, m.need, m.participants, m.steps, m.image, m.id])
+        .then(result => { res.send(result.rows); })
+        .catch(err => {
+            console.log('Error completing GET methods in router', err);
+            res.sendStatus(500);
+        });
+});
+
 router.delete('/:id', (req, res) => {
     let id = req.params.id;
     const queryText = `DELETE FROM method WHERE id=$1`
