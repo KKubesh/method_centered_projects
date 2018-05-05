46378879a2808384427d42a2a8e01816e378c394
Kam Kubesh <kube0057@umn.edu>
2018-05-05 15:01:19 -0500

    request copied for put

--- src/redux/requests/methodRequests.js
+++ src/redux/requests/methodRequests.js
@@ -12,6 +12,12 @@ export function callAddMethod(action) {
     .catch(error => { throw error.reponse || error; });
 }
 
+export function callPutMethod(action) {
+    return axios.put('api/method'+ action.payload.id, action.payload)
+    .then(response => response.data)
+    .catch(error => { throw error.reponse || error; });
+}
+
 export function callDelMethod(action) {
     return axios.delete('api/method/'+ action.payload.id)
     .then(response => response.data)
