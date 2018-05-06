e0427ed333581543f744753d04ebd1336cb36b01
Kam Kubesh <kube0057@umn.edu>
2018-05-06 10:08:31 -0500

    added project reducer to index reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import user from './userReducer';
 import login from './loginReducer';
 import method from './methodReducer';
+import project from './projectReducer';
 
 const store = combineReducers({
   user,
   login,
   method,
+  project,
 });
 
 export default store;
